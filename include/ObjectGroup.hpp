d4019d4f9571ec8665505b96bf138b4bbc24ba38
Borys Szyma <szymaborys@gmail.com>
2017-04-09 23:13:16 +0200

    add ObjectGroup

--- include/ObjectGroup.hpp
+++ include/ObjectGroup.hpp
@@ -2,20 +2,28 @@
 #include "Layer.hpp"
 #include <vector>
 #include <SFML\Graphics\Rect.hpp>
+#include <memory>
 struct ObjectData
 {
-	std::int16_t id;
-	std::int16_t gid;
-	sf::IntRect rect;
+	int16_t id;
+	int16_t gid;
+	float rotation;
+	std::string name;
+	std::string type;
+	sf::FloatRect rect;
 };
 class ObjectGroup :public Layer
 {
 public:
+	using Ptr = std::shared_ptr<ObjectGroup>;
+
 	ObjectGroup();
 	void load(xml_node * node);
+	const std::string & getName()const;
 	const std::vector<ObjectData> & getObject();
 	~ObjectGroup();
 private:
+	std::string name;
 	std::vector<ObjectData> vObject;
 };
 
--- src/ObjectGroup.cpp
+++ src/ObjectGroup.cpp
@@ -1,15 +1,35 @@
 #include "ObjectGroup.hpp"
 #include "ObjectGroup.hpp"
-
+#include <iostream>
 
 
 ObjectGroup::ObjectGroup()
 {
+	setType(TypeLayer::Object);
 }
 
 void ObjectGroup::load(xml_node * node)
 {
+	ObjectData data;
+	name = node->attribute("name").as_string();
+	auto && vobject = node->children("object");
+	vObject.reserve(std::distance(vobject.begin(), vobject.end()));
+	for (auto && obj : vobject)
+	{
+		data.id = obj.attribute("id").as_int();
+		data.gid = obj.attribute("gid").as_int();
+		data.rect = sf::FloatRect(obj.attribute("x").as_float(), obj.attribute("y").as_float(), obj.attribute("width").as_int(), obj.attribute("height").as_int());
+		data.rotation = obj.attribute("rotation").as_float();
+		data.name = obj.attribute("name").as_string();
+		data.type = obj.attribute("type").as_string();
+		vObject.push_back(data);
+	}
 
+}
+
+const std::string & ObjectGroup::getName() const
+{
+	return name;
 }
 
 const std::vector<ObjectData>& ObjectGroup::getObject()
